c2c34dc02cc7fd2d0488bdb1f7207b9fce358862
woarewe <rostislav.zhuravsky@gmail.com>
2017-12-22 02:55:39 +0300

    added slowly scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -112,3 +112,12 @@
     });
 
 })(jQuery);
+
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 750);
+});
