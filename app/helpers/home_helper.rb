faa37a85217d850e167306b0c5ba18c17e726296
woarewe <rostislav.zhuravsky@gmail.com>
2018-01-05 23:24:29 +0300

    added test modal window and fixed bug with choice of color

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,8 +1,9 @@
 module HomeHelper
-  def radio_color_tag(backpack)
+  def radio_color_tag(backpack, checked: false)
     tag = "<div>"
-    tag << "<input type='radio' id=#{backpack.color} name='color' value=#{backpack.color} >"
-    tag << "<label for=#{backpack.color} >"
+    tag << "<input type='radio' id=#{backpack.color} name='color' value=#{backpack.color} "
+    tag << "checked" if checked
+    tag << "><label for=#{backpack.color} >"
     tag << "<span>"
     tag << "<img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/check-icn.svg' alt='Checked Icon' />"
     tag << "</span></label></div>"
