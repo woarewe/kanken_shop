80738540da79b8dca876cbe061c5bdbb101bd8c2
woarewe <rostislav.zhuravsky@gmail.com>
2018-01-05 06:14:51 +0300

    added color panel

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,8 @@
 module HomeHelper
+  def radio_color_tag(backpack)
+    tag = "<div class='color-radio'>"
+    tag << "<input type='radio' id=#{backpack.color} name='radio-group' checked>"
+    tag << "<label for=#{backpack.color}></label></div>"
+    tag
+  end
 end
