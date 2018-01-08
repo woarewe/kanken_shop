12ee5d5f536bee93085201dd10b6ce106186ce5e
woarewe <rostislav.zhuravsky@gmail.com>
2018-01-08 03:04:05 +0300

    added ability to create orders and added admin page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,28 @@
 class OrdersController < ApplicationController
 
+  http_basic_authenticate_with name: 'admin', password: 'admin', except: :create
+
   def destroy
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.save
+
+    respond_to do |format|
+      format.js
+    end
+
   end
 
   def index
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name, :phone, :color, :adress)
+  end
+
 
 end
